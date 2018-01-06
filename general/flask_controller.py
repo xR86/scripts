e35a2f88a98ce91b22d9c39757e5be45a042bd2a
xR86 <9412441+xR86@users.noreply.github.com>
2018-01-06 23:51:24 +0200

    added flask general controller

---
+++ general/flask_controller.py
@@ -0,0 +1,34 @@
+#import tema as tm
+#from . import tema as tm
+
+from flask import Flask
+from flask import render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+	name = 'Flask app'
+	return render_template('mdl_index.html', name=name)
+
+
+# Route handlers
+@app.route('/api/add', methods=['GET']) # works also without methods (default: GET)
+def calculate_add_get_wrapper():
+	return str('Add function, use POST.')
+
+
+@app.route('/api/add', methods=['POST'])
+def calculate_add_post_wrapper():
+	print(request.get_json()) #force=True
+	a = request.get_json()["a"]
+	b = request.get_json()["b"]
+
+	temp = a + b
+	
+	return str(temp)
+
+
+@app.route('/result')
+def smth_wrapper():
+	return str('Not implemented')
