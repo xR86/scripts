82bd3ad7d446b19e89d1b1d5bff12f392dae60b1
xR86 <9412441+xR86@users.noreply.github.com>
2018-04-24 23:54:47 +0300

    updated docker container cleaner

--- Linux/docker/docker_container_cleaner.py
+++ Linux/docker/docker_container_cleaner.py
@@ -1,16 +1,40 @@
-"""docker cleaner"""
+"""docker container cleaner"""
 
 import docker
+import os
+
 client = docker.from_env()
 
-print(client.containers.list(all=True))
 
-# simple txt file, each row is a container name
-with open('.containerignore', 'r') as f:
-	data = f.read().split('\n')
-	ignored = list(filter(None, data))
-	print('data: %s' % data)
+# equivalent of: `docker ps -a`
+def list_containers():
+	print(client.containers.list(all=True))
 
-for container in client.containers.list(all=True):
-	if container.name not in ignored:
-		container.remove()
+def list_ignored():
+	# simple txt file, each row is a container name
+	path = os.path.expanduser('~/.containerignore')
+
+	with open(path, 'r') as f:
+		data = f.read().split('\n')
+		ignored = list(filter(None, data))
+
+	return ignored
+
+def remove_containers():
+	ignored = list_ignored()
+	print('ignored: %s' % ignored)
+
+	removed = []
+	for container in client.containers.list(all=True):
+		if container.name not in ignored:
+			if container.status != 'exited':
+				container.stop()
+			container.remove()
+
+			removed.append(container.name)
+
+	print('removed: %s' % removed)
+
+
+if __name__ == "__main__":
+	remove_containers()
