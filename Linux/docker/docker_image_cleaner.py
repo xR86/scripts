4ac0e5920afed0801eebfb6bc36aac45c1e4d4e5
xR86 <9412441+xR86@users.noreply.github.com>
2018-04-25 01:37:51 +0300

    updated docker image cleaner

--- Linux/docker/docker_image_cleaner.py
+++ Linux/docker/docker_image_cleaner.py
@@ -1,8 +1,11 @@
 """docker image cleaner"""
+
 import docker
+import os
 import sys
 
 client = docker.from_env()
+
 
 # equivalent of: `docker images`
 def list_images():
@@ -10,27 +13,54 @@ def list_images():
 		"dangling": True
 	}))
 
+def list_ignored():
+	# simple txt file, each row is a image name
+	path = os.path.expanduser('~/.imageignore')
 
-if len(sys.argv) > 1:
+	with open(path, 'r') as f:
+		data = f.read().split('\n')
+		ignored = list(filter(None, data))
 
-	list_images()
-	
+	return ignored
+
+def remove_images():
+	ignored = list_ignored()
+	print('ignored: %s' % ignored)
+
+	remove_images_safe()
+
+	removed = []
+	for image in client.images.list(all=True):
+		img_name = '' if len(image.tags) < 1 else image.tags[0]
+		img_id = image.id
+		print('name: %s,\n id: %s' % (img_name, img_id))
+
+		if img_name not in ignored:
+			# remove fails: https://stackoverflow.com/a/26692854
+			client.images.remove(image=img_id, force=True)
+
+			removed.append(img_name)
+
+	print('removed: %s' % removed)
+
+def remove_images_safe():
 	# --safe - using built-in prune method, 
 	#          it will delete unused images only
-	if sys.argv[1] == '--safe':
-		dct = client.images.prune()
-		print(dct)
+	list_images()
 
-		list_images()
-else:
-	print('No cleaning method chose')
+	# similar command
+	# docker rmi $(docker images -f "dangling=true" -q)
+	pruned = client.images.prune(filters={
+		"dangling": True
+	})
+	print('pruned: %s' % pruned)
 
-# # simple txt file, each row is a container name
-# with open('~/.imageignore', 'r') as f:
-# 	data = f.read().split('\n')
-# 	ignored = list(filter(None, data))
-# 	print('data: %s' % data)
+	list_images()
 
-# for image in client.images.list(all=True):
-# 	if image.name not in ignored:
-# 		image.remove()
+
+if __name__ == "__main__":
+	if len(sys.argv) > 1:
+		if sys.argv[1] == '--safe':
+			remove_images_safe()
+	else:
+		remove_images()
